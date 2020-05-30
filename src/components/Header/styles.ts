f95a61335031b5694a089f448ede8d223c5cfbac
Renan <rollorenan@gmail.com>
2020-05-30 19:03:18 -0300

    NavLink e format with Intl function

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,7 +9,7 @@ export const Container = styled.div<ContainerProps>`
   padding: 30px 0;
 
   header {
-    width: 1120px;
+    max-width: 1120px;
     margin: 0 auto;
     padding: ${({ size }) => (size === 'small' ? '0 20px ' : '0 20px 150px')};
     display: flex;
@@ -17,11 +17,16 @@ export const Container = styled.div<ContainerProps>`
     justify-content: space-between;
 
     nav {
+      > a.active {
+        border-bottom: 1px solid #ff872c;
+      }
+
       a {
         color: #fff;
         text-decoration: none;
         font-size: 16px;
         transition: opacity 0.2s;
+        padding-bottom: 8px;
 
         & + a {
           margin-left: 32px;
